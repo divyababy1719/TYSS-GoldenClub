ff7a290645dbb406a4fdfa61e06b5652de438ca2
JOLLU KISHORE <JOLLU KISHORE@DESKTOP-IH4C39S>
2022-07-18 13:17:23 +0530

    New project shared by Divya

---
+++ src/test/java/CreateContactTest.java
@@ -0,0 +1,17 @@
+
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+@Test(groups="smoke")
+public void createContact()
+{
+	System.out.println("Contact Created");
+}
+@Test(groups="regression")
+public void modifyContact()
+{
+	System.out.println("Contact Modified");
+}
+
+}
