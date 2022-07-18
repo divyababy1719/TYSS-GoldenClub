447cc01e3a72e5f45dab3c3f1d5bed956d927162
naveen magnur <naveen magnur@magnur>
2022-07-18 13:27:42 +0530

    Added by sanjay

--- src/test/java/CreateContactTest.java
+++ src/test/java/CreateContactTest.java
@@ -7,11 +7,13 @@ public class CreateContactTest {
 public void createContact()
 {
 	System.out.println("Contact Created");
+	System.out.println("This statment is added by sanjay in createcontact");
 }
 @Test(groups="regression")
 public void modifyContact()
 {
 	System.out.println("Contact Modified");
+	System.out.println("This statment is added by sanjay in modifycontact");
 }
 
 }
