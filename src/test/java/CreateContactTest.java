b7ab8490e23080ac55b7637a40c8e8466be2355f
Admin <Admin@DESKTOP-HC1J7FO>
2022-07-18 13:38:39 +0530

    Second Changed by chandu

--- src/test/java/CreateContactTest.java
+++ src/test/java/CreateContactTest.java
@@ -8,12 +8,14 @@ public void createContact()
 {
 	System.out.println("Contact Created");
 	
-	System.out.println("Hi chandan");
+	System.out.println("Hi chandu");
 }
 @Test(groups="regression")
 public void modifyContact()
 {
 	System.out.println("Contact Modified");
+	
+	System.out.println("Bye chandu");
 }
 
 }
