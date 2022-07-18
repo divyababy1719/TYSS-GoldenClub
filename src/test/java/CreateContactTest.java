b8bb83397c8cc28ab1a47dbfebf0a037944779fd
sheno <sheno@HVSL>
2022-07-18 13:30:32 +0530

    This is first commit

--- src/test/java/CreateContactTest.java
+++ src/test/java/CreateContactTest.java
@@ -12,6 +12,7 @@ public void createContact()
 public void modifyContact()
 {
 	System.out.println("Contact Modified");
+	System.out.println("Soumya is changing the code for the first time");
 }
 
 }
